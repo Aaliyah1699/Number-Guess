1eee59fd1ec1a0f2ddb0a4d9c4773c7d9782aec2
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-02-01 23:52:45 +0000

    started writing comments for game

---
+++ guess.py
@@ -0,0 +1,8 @@
+'''Number Guessing  Game'''
+import random
+# Empty attemps list
+'''1st Function to show score with -> if not / else linking to attempts list with 2 prints + print 1 No high score so user wins + print 2 f string {min{attempts}} showing the current high score'''
+
+'''2nd Function to start game with attempts var, and random var = random.randint(range,nums) + print welcoming user to game and asking user for name then use f str asking {user} if they want to play'''
+
+'''1st if statement -> if answer yes - print reply and then exit() else call 1st function name w/ ()'''
