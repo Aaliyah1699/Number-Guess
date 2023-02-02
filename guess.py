da631a9dab2744945153efaf994b581b0c4ed50d
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-02-02 05:00:18 +0000

    made comment for ran num detailed

--- guess.py
+++ guess.py
@@ -3,6 +3,7 @@ import random
 # Empty attempts list
 attempts_list = []
 '''1st Function to show score with -> if not / else linking to attempts list with 2 prints + print 1 No high score so user wins + print 2 f string {min(attempts)} showing the current high score'''
+
 def show_score():
     if not attempts_list:
         print("There is no high score, you win!")
@@ -10,9 +11,11 @@ def show_score():
         print(f"The current high score is {min(attempts_list)} attempts")
 
 '''2nd Function to start game with attempts var, and random var = random.randint(range,nums) + print welcoming user to game and asking user for name then use f str asking {user} if they want to play'''
+
 def game_start():
     # variable to hold the number of attempts starting at 0
     attempts = 0
-    # pick a random number
+    # pick a random number between 100 - 120
     ran_num = random.randint(100, 120)
+
 '''1st if statement -> if answer yes - print reply and then exit() else call 1st function name w/ ()'''
